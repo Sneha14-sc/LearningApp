c25a7453f3c9e40f5199346209e6575168844908
leespgre <leespgre@gmail.com>
2017-09-29 14:32:05 +0900

    Creat new angular-svg-round-progressbar - Angular and click button to move to question state

--- client/src/hxLearningApp/learningProgressDisp.controller.js
+++ client/src/hxLearningApp/learningProgressDisp.controller.js
@@ -5,23 +5,32 @@
         "use strict";
         angular.module('hxLearningApp')
             .controller('learningProgressController', learningProgressController);
-        learningProgressController.$inject = ['learningAppDataService'];
-        function learningProgressController(learningAppDataService) {
+        learningProgressController.$inject = ['$state','learningAppDataService'];
+        function learningProgressController($state,learningAppDataService) {
             var $leProDi = this;
             $leProDi.printedCheck="Check learningProgressController ok!";
             console.log('inside the learningProgressController');
             $leProDi.correctNumber=learningAppDataService.numberCorrect;
             $leProDi.totalNumber=5;
-            $leProDi.drawData=[
-                ['correctNumber',$leProDi.correctNumber],
-                ['other',$leProDi.totalNumber-$leProDi.correctNumber]
-            ];
+            // $leProDi.drawData=[
+            //     ['correctNumber',$leProDi.correctNumber],
+            //     ['other',$leProDi.totalNumber-$leProDi.correctNumber]
+            // ];
+            $leProDi.drawData= {
+              correctNumber : $leProDi.correctNumber,
+                totalNumber: $leProDi.totalNumber
+            };
+
+            console.log('drawData=',$leProDi.drawData);
 
             // $leProDi.drawData=[
             //     ['correctNumber',1],
             //     ['totalNumber',2]
             // ];
             $leProDi.drawTitle="Study Status";
+            $leProDi.changeState=function () {
+                $state.go('hxLearningApp.learningQuestions');
+            }
         }
     }
 )();
